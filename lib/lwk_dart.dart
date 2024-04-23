84c2fbdf44297a17b905f81746aa6f677598aa31
i5hi <vishalmenon.92@gmail.com>
2024-04-23 10:28:29 +0530

    Exported generated/api/*

--- lib/lwk_dart.dart
+++ lib/lwk_dart.dart
@@ -1,5 +1,7 @@
 library lwk_dart;
 
 export './src/generated/frb_generated.dart';
-
+export './src/generated/api/descriptor.dart';
+export './src/generated/api/wallet.dart';
+export './src/generated/api/types.dart';
 export './src/root.dart';
