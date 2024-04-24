31aef8b7fda1d19483ecda9fdde70cdbca9495ca
joaolobo <joao.lobom@gmail.com>
2024-04-24 13:46:35 +0100

    add error to exports

--- lib/lwk_dart.dart
+++ lib/lwk_dart.dart
@@ -2,6 +2,7 @@ library lwk_dart;
 
 export './src/generated/frb_generated.dart';
 export './src/generated/api/descriptor.dart';
+export './src/generated/api/error.dart';
 export './src/generated/api/wallet.dart';
 export './src/generated/api/types.dart';
 export './src/root.dart';
