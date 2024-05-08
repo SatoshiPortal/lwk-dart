f2cc3a78c793304e2d6d025fb378b92d014d5640
joaolobo <joao.lobom@gmail.com>
2024-05-08 09:11:04 +0100

    delete error handling

--- lib/lwk_dart.dart
+++ lib/lwk_dart.dart
@@ -2,7 +2,6 @@ library lwk_dart;
 
 export './src/generated/frb_generated.dart';
 export './src/generated/api/descriptor.dart';
-export './src/generated/api/error.dart';
 export './src/generated/api/wallet.dart';
 export './src/generated/api/types.dart';
 export './src/root.dart';
