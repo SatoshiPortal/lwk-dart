db2e8cd4f69687aedd8295e71bf356ad6c77b18e
joaolobo <joao.lobom@gmail.com>
2024-05-08 09:13:01 +0100

    fix compilation

--- compile.native.sh
+++ compile.native.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
 mkdir -p build/unit_test_assets
 cd rust || exit 1
 cargo update
-cargo build --release --target=aarch64-apple-darwin
+cargo build --release
 OS=$(uname -s)
 if [ "$OS" = "Linux" ]; then
-    cp target/aarch64-apple-darwin/release/liblwkbridge.so ../build/unit_test_assets
+    cp target/release/liblwkbridge.so ../build/unit_test_assets
 elif [ "$OS" = "Darwin" ]; then
-    cp target/aarch64-apple-darwin/release/liblwkbridge.dylib ../build/unit_test_assets
+    cp target/release/liblwkbridge.dylib ../build/unit_test_assets
 else
     echo "Unsupported OS: $OS"
     exit 1
-fi
-cd - || exit 1
